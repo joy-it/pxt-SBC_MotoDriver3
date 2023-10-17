6d2b607a41b5e464720d62fc75028c8acef58f2e
L0l0 Joy-IT <96109036+L0l0-tack@users.noreply.github.com>
2023-10-17 15:05:09 +0200

    Update SBC_MotoDriver3.ts

--- SBC_MotoDriver3.ts
+++ SBC_MotoDriver3.ts
@@ -196,7 +196,7 @@ namespace SBC_MotoDriver3 {
     /**
      * Turn on a single channel (accepts values between 0 and 7)
      */
-    //% blockId="SBC_MotoDriver3_ON" block="Turn on channel |%pin\\"
+    //% blockId="SBC_MotoDriver3_ON" block="Turn on |channel %pin|\\"
     //% pin.min= 0 pin.max=7 pin.defl=0
     //% color="#275C6B" weight=85 blockGap=8
     //% parts=led_SBC_MotoDriver3 trackArgs=0
