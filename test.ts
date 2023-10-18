ae60ad487d7b4143cea986c898481224ce25c875
L0l0f13r <t.muehlbrandt@simac-gmbh.de>
2023-10-18 09:46:17 +0200

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,6 +1,6 @@
 // Initialization of the board with I2C address 0x15 and oe_pin 8 and pull the oe_pin low to activate the board
 SBC_MotoDriver3.initSBC_MotoDriver3(21)
-SBC_MotoDriver3.enable(SBC_MotoDriver3pin.P8, true)
+SBC_MotoDriver3.enable(SBC_MotoDriver3pin.P8, SBC_MotoDriver3state.On)
 // Switch off all outputs
 SBC_MotoDriver3.allOff()
 // Define the RPM and the maximum number of steps for the stepper motor
